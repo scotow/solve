fb31728c89fc152ba49133d223b94461912d6fcd
Scotow <benjamin-lopez@hotmail.fr>
2016-12-01 16:28:13 +0100

    Started colisions

--- js/solve.js
+++ js/solve.js
@@ -54,7 +54,26 @@ $(function(){
     var map = {
         width: canvas.width,
         height: canvas.height
+        shapes: []
     };
+
+    function Shape(x, y, width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.isInside = function(x, y, width, height){
+                    if (rect1.x < rect2.x + rect2.width &&
+           rect1.x + rect1.width > rect2.x &&
+           rect1.y < rect2.y + rect2.height &&
+           rect1.height + rect1.y > rect2.y) {
+            // collision detected!
+        }
+
+        return this.x < x + width
+        }
+    }
 
     var player = {
         width: 75,
